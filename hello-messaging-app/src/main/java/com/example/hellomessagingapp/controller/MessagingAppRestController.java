a987f59b3ada0dc21f5320bd611790a08641bda6
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-01 23:42:14 +0530

    Added: get method query param to demonstrate REST API usin CURL

--- hello-messaging-app/src/main/java/com/example/hellomessagingapp/controller/MessagingAppRestController.java
+++ hello-messaging-app/src/main/java/com/example/hellomessagingapp/controller/MessagingAppRestController.java
@@ -1,6 +1,8 @@
 package com.example.hellomessagingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,12 @@ public class MessagingAppRestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+    @GetMapping("/hello/query")
+    public String helloWithName(
+            @RequestParam(value = "name") String name
+    ) {
+        return "Hello " + name + " from BridgeLabz";
+
     }
 }
