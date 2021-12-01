e03a4ac7b7b73aeda099037ae02b798e9e78bce4
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-02 01:14:16 +0530

    Added: post method by create DTO to demonstrate REST API using CURL

--- hello-messaging-app/src/main/java/com/example/hellomessagingapp/controller/MessagingAppRestController.java
+++ hello-messaging-app/src/main/java/com/example/hellomessagingapp/controller/MessagingAppRestController.java
@@ -1,5 +1,6 @@
 package com.example.hellomessagingapp.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,5 +20,17 @@ public class MessagingAppRestController {
     @GetMapping("/hello/param/{name}")
     public String helloWithParamName(@PathVariable String name) {
         return "Hello " +name +" from bridgelabz";
+    }
+
+    /**
+     *  posting data to server using json
+     *  curl -X POST http://localhost:8080/hello/post -H 'Cache-control: no-choice' -H 'content-type: application/json'
+     *  -d '{"firstName":"Mark", "lastName":"Taylor"}'
+     * @param requestUserDetails
+     * @return
+     */
+    @PostMapping(value = "/hello/post", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public String helloWithPostMethod(@RequestBody UserDTO requestUserDetails) {
+        return "Hello "+requestUserDetails.getFirstName()+ " " +requestUserDetails.getLastName()+ " from brigdgelabz";
     }
 }
