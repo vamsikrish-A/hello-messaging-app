9318a38e9fd9270ef1d37a8a145250a381a99b3b
vamsikrish-A <krishnavamsi165@gmail.com>
2021-12-01 23:05:01 +0530

    Added: Spring Files with dependecies, server running on port-8080

---
+++ hello-messaging-app/src/main/java/com/example/hellomessagingapp/HelloMessagingAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.hellomessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to BridgeLabZ");
+		SpringApplication.run(HelloMessagingAppApplication.class, args);
+	}
+
+}
